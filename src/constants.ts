178138761caff7090922d4f153142a0b6af42a0c
Alice <alice@beforeyoubid.com.au>
2023-02-13 18:59:25 +1100

    fixed doc path

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * generate the path to the docs file for the rule
+ * @param name the rule's name
+ * @returns a URL to the doc path
+ */
+export const DOC_PATH = (name: string) =>
+  `https://github.com/bailey-byb/eslint-plugin-avoid-jest-spy-on/blob/main/docs/rules/${name}.md`;
