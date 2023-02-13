2b0fcd7f68ec80a9da47f5c65392486e96db3729
Bailey Sheather <bailey@beforeyoubid.com.au>
2023-02-13 19:10:29 +1100

    fixed dry run again

--- module_test/.eslintrc.js
+++ module_test/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
   },
   rules: {
-    'avoid-jest-spy-on/avoid-jest-spyOn': 'error',
+    'avoid-jest-spy-on/avoid-jest-spyOn': 'warn',
   },
   plugins: ['avoid-jest-spy-on'],
 };
